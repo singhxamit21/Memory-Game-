4fe126215761df9122573d46c55415276e59c913
Amit Singh <62277948+singhxamit21@users.noreply.github.com>
2021-09-04 18:31:15 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+import IMG1 from "./image/avenger1.jpg";
+import IMG2 from "./image/avenger2.jpg";
+import IMG3 from "./image/avenger3.jpg";
+import IMG4 from "./image/avenger4.jpg";
+import IMG5 from "./image/avenger5.jpg";
+import IMG6 from "./image/avenger6.jpg";
+import IMG7 from "./image/avenger7.jpg";
+import IMG8 from "./image/avenger8.jpg";
+import IMG9 from "./image/avenger9.jpg";
+
+const uniqueCardsArray = [
+  {
+    type: "avenger3",
+    image: IMG1,
+  },
+  {
+    type: "avenger5",
+    image: IMG2,
+  },
+  {
+    type: "avenger6",
+    image: IMG3,
+  },
+  {
+    type: "avenger7",
+    image: IMG4,
+  },
+  {
+    type: "avenger8",
+    image: IMG5,
+  },
+  {
+    type: "avenger2",
+    image: IMG6,
+  },
+  {
+    type: "avenger4",
+    image: IMG7,
+  },
+  {
+    type: "avenger9",
+    image: IMG8,
+  },
+  {
+    type: "avenger1",
+    image: IMG9,
+  },
+];
+
+export default uniqueCardsArray;
